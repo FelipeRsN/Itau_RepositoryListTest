a6820656026f0bb901e6c9a3eba8d8cb9036968e
Felipe Silveira <felipe@appito.club>
2019-12-11 23:47:36 -0300

    first activity created. Project ready to start.

---
+++ app/src/main/java/com/felipersn/itaurepositorylist/presentation/repositorylist/RepositoryListActivity.kt
@@ -0,0 +1,13 @@
+package com.felipersn.itaurepositorylist.presentation.repositorylist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.felipersn.itaurepositorylist.R
+
+class RepositoryListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_repository_list)
+    }
+}
